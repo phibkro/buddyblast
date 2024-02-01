8567065bcd9afc1429ec1fa89fabc39158633994
philipbk <philipbk@stud.ntnu.no>
2024-02-01 16:22:30 +0100

    feat: :sparkles: set up shadcn/ui

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,3 +1,5 @@
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
 import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 
@@ -6,13 +8,13 @@ export const Route = createRootRoute({
     <>
       <div className="p-2 flex gap-2">
         <Link to="/" className="[&.active]:font-bold">
-          Home
-        </Link>{" "}
+          <Button>Home</Button>
+        </Link>
         <Link to="/about" className="[&.active]:font-bold">
-          About
+          <Button>About</Button>
         </Link>
       </div>
-      <hr />
+      <Separator />
       <Outlet />
       <TanStackRouterDevtools />
     </>
