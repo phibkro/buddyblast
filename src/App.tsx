d69cade68f7576e3106190a70c034d1b123b690b
philipbk <philipbk@stud.ntnu.no>
2024-02-01 16:38:31 +0100

    feat: :sparkles: set up prettier plugins

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React, { StrictMode } from "react";
-import ReactDOM from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
 import "./global.css";
 
 // Import the generated route tree
@@ -23,6 +23,6 @@ if (!rootElement.innerHTML) {
   root.render(
     <StrictMode>
       <RouterProvider router={router} />
-    </StrictMode>
+    </StrictMode>,
   );
 }
