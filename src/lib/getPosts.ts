ece5ec8d999c2cbc08afa42b8db528d23d6d34c6
Philip Bjørknes Krogh <philipbk@stud.ntnu.no>
2024-02-19 12:43:24 +0100

    feat: :sparkles: fetch data with getPosts in PostsFeed with loader

--- src/lib/getPosts.ts
+++ src/lib/getPosts.ts
@@ -1,18 +1,12 @@
 import { db } from "@/lib/firebase";
 import { collection, getDocs } from "@firebase/firestore";
 export async function getPosts() {
-    // henter alle brukere der glasses er lik yes - kan byttes til kategori senere
+  const postsCollection = collection(db, "posts");
+  const querySnapshot = await getDocs(postsCollection);
 
-    // const q = query(
-    //     collection(db, "posts"),
-    //     where("postTitle", "==", "test post"),
-    // );
-    const postsCollection = collection(db, "posts");
-    const querySnapshot = await getDocs(postsCollection);
+  const posts = querySnapshot.docs.map((doc) => {
+    return { id: doc.id, ...doc.data() };
+  });
 
-    const posts = querySnapshot.docs.map(doc => {
-        return { id: doc.id, ...doc.data() };
-    });
-
-    return posts;
+  return posts;
 }
