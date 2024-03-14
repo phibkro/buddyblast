1b2bd8fe5297070fbdf0ecc01f7d3ac4e39de769
amalsj <amalisj@stud.ntnu.no>
2024-03-14 13:09:17 +0100

    style: :lipstick: Change padding and timer styling

--- src/routes/post.tsx
+++ src/routes/post.tsx
@@ -32,12 +32,13 @@ export function Post() {
   }
 
   return (
-    <div>
-      <div className="ml-5 flex justify-between">
+    <div className="flex items-center p-24">
+      <div className="flex items-center justify-between space-x-20">
         <SinglePost entry={post} />
-      </div>
-      <div>
-        <Timer startTime={post.defaultTime} isPLaying={false} />
+        <div className="flex flex-col items-center justify-between">
+          <h1 className="pb-4 text-lg font-bold">COUNTDOWN</h1>
+          <Timer startTime={post.defaultTime} isPLaying={false} />
+        </div>
       </div>
     </div>
   );
